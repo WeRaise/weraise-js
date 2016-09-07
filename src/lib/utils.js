27a5d4d33a52e948b8732e87e6c8f7008eed8225
Daniel Jones <dj311@users.noreply.github.com>
2016-09-07 15:49:51 +0100

    Added a fn to capitilize the first letter of a string (#8)

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -50,5 +50,9 @@ var utils = (function() {
     return url;
   };
 
+  exports.capitalizeFirstLetter = function(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  };
+
   return exports;
 })();
